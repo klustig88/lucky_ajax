0ebde8be2d9440d605129118dfe67fd743586b07
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-04-23 10:48:15 -0700

    initial commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,13 @@
+get '/' do
+  erb :index
+end
+
+post '/rolls' do
+  # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
+  # See: roll_if_value_is_nil method in the Roll model.
+  value = params[:value] ? params[:value].to_i : nil
+
+  @roll = value ? Roll.create({ value: value }) : Roll.create
+
+  erb :index
+end
